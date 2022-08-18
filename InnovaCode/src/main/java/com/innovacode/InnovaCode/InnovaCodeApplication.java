290cb15d2e49fc2adc04266a4cf0b01cff43850a
Jorge Leonardo Guepud Bravo <jlguepudb@unal.edu.co>
2022-08-17 22:47:11 -0500

    Se modifica el proyecto para usar Spring

---
+++ InnovaCode/src/main/java/com/innovacode/InnovaCode/InnovaCodeApplication.java
@@ -0,0 +1,15 @@
+package com.innovacode.InnovaCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaCodeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(InnovaCodeApplication.class, args);
+		System.out.println("Hello world InnovaCode");
+	}
+
+}
---
+++ InnovaCode/src/test/java/com/innovacode/InnovaCode/InnovaCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovacode.InnovaCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
