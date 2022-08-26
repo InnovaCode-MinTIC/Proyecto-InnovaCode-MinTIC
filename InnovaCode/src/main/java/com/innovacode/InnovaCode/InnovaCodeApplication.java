fdff0d5089047c8b6917b8336ae5abbd5c099a79
Jorge Leonardo Guepud Bravo <jlguepudb@unal.edu.co>
2022-08-25 21:19:59 -0500

    Cambios estéticos en la consola para la función testingEnterprise

--- InnovaCode/src/main/java/com/innovacode/InnovaCode/InnovaCodeApplication.java
+++ InnovaCode/src/main/java/com/innovacode/InnovaCode/InnovaCodeApplication.java
@@ -21,28 +21,33 @@ public class InnovaCodeApplication {
 
 		//Se inicializa cada ítem de la lista y automáticamente se crean 10 empresas.
 		for (int i = 0; i<10 ; i++) {
-			enterprises.add(new Enterprise(i,"Empresa "+i,""+i+i+i+"."+i+i+i+"."+i+i+i+"-"+i,"+57 "+i+i+i+i+i+i+i+i+i+i,"Calle "+i+" #"+i+" - "+i));
+			enterprises.add(new Enterprise(i,"Empresa "+i,""+i+i+i+"."+i+i+i+"."+i+i+i+"-"+i,
+					"+57 "+i+i+i+i+i+i+i+i+i+i,"Calle "+i+" #"+i+" - "+i));
 		}
 
 		//Se lee y modifica el nombre de la empresa en 1.
 		System.out.println("Nombre original: " + enterprises.get(1).getName());
 		enterprises.get(1).setName("Empresa nueva");
 		System.out.println("Nuevo nombre: " + enterprises.get(1).getName());
+		System.out.println();
 
 		//Se lee y modifica la dirección de la empresa en 2.
-		System.out.println("Direccion original: "+ enterprises.get(2).getAddress());
-		enterprises.get(2).setAddress("Carrera 1 # 2 - 3");
-		System.out.println("Nueva Direccion: " + enterprises.get(2).getAddress());
+		System.out.println("Direccion original: "+ enterprises.get(1).getAddress());
+		enterprises.get(1).setAddress("Carrera 1 # 2 - 3");
+		System.out.println("Nueva Direccion: " + enterprises.get(1).getAddress());
+		System.out.println();
 
 		//Se lee y modifica el teléfono de la empresa en 0.
 		System.out.println("Telefono original: " + enterprises.get(0).getPhone());
 		enterprises.get(0).setPhone("+57 1234567890");
 		System.out.println("Nuevo telefono: " + enterprises.get(0).getPhone());
+		System.out.println();
 
 		//Se lee y modifica la identificación (NIT) de la empresa en 8.
 		System.out.println("Identificacion original: " + enterprises.get(8).getDocument());
 		enterprises.get(8).setDocument("123.456.789-0");
 		System.out.println("Nueva Identificacion: " + enterprises.get(8).getDocument());
+		System.out.println();
 	}
 
 }
