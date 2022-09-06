b7be51d942e036c597d925c76fb6c93b2fb7d958
Jorge Leonardo Guepud Bravo <jlguepudb@unal.edu.co>
2022-09-05 23:22:03 -0500

    CRUD con HTML

--- InnovaCode/src/main/java/com/innovacode/InnovaCode/controller/EmployeeController.java
+++ InnovaCode/src/main/java/com/innovacode/InnovaCode/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.innovacode.InnovaCode.controller;
 
 import com.innovacode.InnovaCode.entity.Employee;
+import com.innovacode.InnovaCode.entity.Enterprise;
 import com.innovacode.InnovaCode.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,29 +16,71 @@ public class EmployeeController {
     @Autowired
     private EmployeeService service;
 
-    @GetMapping("/users")
-    public List<Employee> employeeList(){
-        return this.service.listEmployee();
+//    @GetMapping("/users")
+//    public List<Employee> employeeList(){
+//        return this.service.listEmployee();
+//    }
+//
+//    @PostMapping("/users")
+//    public Employee createEmployee(@RequestBody Employee employee){
+//        return this.service.saveEmployee(employee);
+//    }
+//
+//    @GetMapping("/users/{id}")
+//    public Employee employeeById(@PathVariable Long id){
+//        return this.service.getEmployee(id);
+//    }
+//
+//    @PatchMapping("/users/{id}")
+//    public Employee updateEmployee(@PathVariable Employee employee){
+//        return this.service.updateEmployee(employee);
+//    }
+//
+//    @DeleteMapping("/users/{id}")
+//    public void deleteEmployeeById(@PathVariable Long id){
+//        this.service.deleteEmployee(id);
+//    }
+
+
+    @GetMapping({"/employee"})
+    public String listarEmpleados(Model modelo){
+        modelo.addAttribute("employee", service.listEmployee());
+        return "EmployeeList";
+    }
+    @GetMapping("/employee/create")
+    public String FormCreateEmployee(Model modelo){
+        Employee employee = new Employee();
+        modelo.addAttribute("employee", employee);
+        return "EmployeeCreate";
     }
 
-    @PostMapping("/users")
-    public Employee createEmployee(@RequestBody Employee employee){
-        return this.service.saveEmployee(employee);
+    @PostMapping("/employee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        service.saveEmployee(employee);
+        return "redirect:/employee";
     }
 
-    @GetMapping("/users/{id}")
-    public Employee employeeById(@PathVariable Long id){
-        return this.service.getEmployee(id);
+    @GetMapping("/employee/edit/{id}")
+    public String mostrarFormularioEditar(@PathVariable Long id, Model modelo) {
+        modelo.addAttribute("employee", service.getEmployee(id));
+        return "employeeEdit";
     }
 
-    @PatchMapping("/users/{id}")
-    public Employee updateEmployee(@PathVariable Employee employee){
-        return this.service.updateEmployee(employee);
+    @PostMapping("/employee/{id}")
+    public String updateEmployee(@PathVariable Long id,@ModelAttribute("employee") Employee employee,Model modelo){
+        Employee employeeExistente = service.getEmployee(id);
+        employeeExistente.setId(id);
+        employeeExistente.setEmail(employee.getEmail());
+        employeeExistente.setProfile(employee.getProfile());
+        employeeExistente.setRole(employee.getRole());
+        employeeExistente.setEnterprise(employee.getEnterprise());
+        service.updateEmployee(employeeExistente);
+        return "redirect:/employee";
     }
 
-    @DeleteMapping("/users/{id}")
-    public void deleteEmployeeById(@PathVariable Long id){
-        this.service.deleteEmployee(id);
+    @GetMapping("/employee/{id}")
+    public String deleteEnterprises(@PathVariable Long id) {
+        service.deleteEmployee(id);
+        return  "redirect:/enterprises";
     }
-
 }
