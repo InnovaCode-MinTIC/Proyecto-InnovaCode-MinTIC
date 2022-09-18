016699b6395148e630facccf9697d7de4b7254b5
Jorge Leonardo Guepud Bravo <jlguepudb@unal.edu.co>
2022-09-18 15:40:45 -0500

    Se actualizan los controladores y se crean las UI para users y movements

--- InnovaCode/src/main/java/com/innovacode/InnovaCode/controllers/EmployeeController.java
+++ InnovaCode/src/main/java/com/innovacode/InnovaCode/controllers/EmployeeController.java
@@ -2,34 +2,74 @@ package com.innovacode.InnovaCode.controllers;
 
 import com.innovacode.InnovaCode.entities.Employee;
 import com.innovacode.InnovaCode.services.EmployeeService;
+import org.h2.engine.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class EmployeeController {
 
     @Autowired
     private EmployeeService service;
 
+//    @GetMapping("/users")
+//    public List<Employee> getEmployeeList(){
+//        return this.service.getEmployeeList();
+//    }
+//    @PostMapping("/users")
+//    public Employee postEmployee(@RequestBody Employee employee){
+//        return this.service.postEmployee(employee);
+//    }
+//    @GetMapping("/users/{id}")
+//    public Employee getEmployeeById(@PathVariable Long id){
+//        return this.service.getEmployeeById(id);
+//    }
+//    @PatchMapping("/users/{id}")
+//    public Employee patchEmployee(@PathVariable Long id,@RequestBody Employee employee){
+//        return this.service.patchEmployee(id,employee);    }
+//    @DeleteMapping("/users/{id}")
+//    public void deleteEmployee(@PathVariable Long id){
+//        this.service.deleteEmployee(id);
+//    }
+
     @GetMapping("/users")
-    public List<Employee> getEmployeeList(){
-        return this.service.getEmployeeList();
+    public String ListUsers(Model model){
+        model.addAttribute("employees", this.service.getEmployeeList());
+        return "UsersList";
     }
+
+    @GetMapping("/users/create")
+    public String FormCreateUser (Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "UsersCreate";
+    }
+
     @PostMapping("/users")
-    public Employee postEmployee(@RequestBody Employee employee){
-        return this.service.postEmployee(employee);
+    public String saveUser(@ModelAttribute("employee") Employee employee){
+        this.service.postEmployee(employee);
+        return "redirect:/users";
     }
+
+    @GetMapping("/users/edit/{id}")
+    public String FormEditUser(@PathVariable Long id, Model model){
+        model.addAttribute("employee", this.service.getEmployeeById(id));
+        return "UsersEdit";
+    }
+
+    @PostMapping("/users/{id}")
+    public String patchUser(@PathVariable Long id, @ModelAttribute("employee") Employee employee, Model model){
+        this.service.patchEmployee(id, employee);
+        return "redirect:/users";
+    }
+
     @GetMapping("/users/{id}")
-    public Employee getEmployeeById(@PathVariable Long id){
-        return this.service.getEmployeeById(id);
-    }
-    @PatchMapping("/users/{id}")
-    public Employee patchEmployee(@PathVariable Long id,@RequestBody Employee employee){
-        return this.service.patchEmployee(id,employee);    }
-    @DeleteMapping("/users/{id}")
-    public void deleteEmployee(@PathVariable Long id){
+    public String deleteUser(@PathVariable Long id){
         this.service.deleteEmployee(id);
+        return "redirect:/users";
     }
 }
--- InnovaCode/src/main/java/com/innovacode/InnovaCode/controllers/TransactionController.java
+++ InnovaCode/src/main/java/com/innovacode/InnovaCode/controllers/TransactionController.java
@@ -1,36 +1,85 @@
 package com.innovacode.InnovaCode.controllers;
 
+import com.innovacode.InnovaCode.entities.Employee;
 import com.innovacode.InnovaCode.entities.Transaction;
 import com.innovacode.InnovaCode.services.TransactionService;
+import org.h2.engine.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.desktop.PreferencesEvent;
 import java.util.List;
 
-@RestController
+@Controller
 public class TransactionController {
     @Autowired
     private TransactionService service;
+//    @GetMapping("/movements")
+//    public List<Transaction> getTransactionList(){
+//        return this.service.getTransactionList();
+//    }
+//    @PostMapping("/movements")
+//    public Transaction postTransaction(@RequestBody Transaction transaction){
+//        return this.service.postTransaction(transaction);    }
+//    @GetMapping("/movements/{id}")
+//    public Transaction getTransactionById(@PathVariable Long id){
+//        return this.service.getTransactionById(id);
+//    }
+//    @PatchMapping("/movements/{id}")
+//    public Transaction patchTransaction(@PathVariable Long id, @RequestBody Transaction transaction){
+//        return this.service.patchTransaction(id,transaction);    }
+//    @DeleteMapping("/movements/{id}")
+//    public void deleteTransaction(@PathVariable Long id){
+//        this.service.deleteTransaction(id);
+//    }
+//    @GetMapping("/enterprises/{id}/movements")
+//    public List<Transaction> getTransactionByEnterprise(@PathVariable Long id){
+//        return this.service.getTransactionByEnterprise(id);
+//    }
+
+//    @GetMapping("/enterprises/{id}/movements")
+//    public String ListarMovimientos(Model model,@PathVariable Long id){
+//        model.addAttribute("enterprises", service.getTransactionByEnterprise(id));
+//        return "MovementsList";
+//    }
+
     @GetMapping("/movements")
-    public List<Transaction> getTransactionList(){
-        return this.service.getTransactionList();
+    public String ListMovements(Model model){
+        model.addAttribute("transactions", this.service.getTransactionList());
+        return "MovementsList";
     }
+
+    @GetMapping("/movements/create")
+    public String FormCreateMovement(Model model){
+        Transaction transaction = new Transaction();
+        model.addAttribute("transaction",this.service.postTransaction(transaction));
+        return "MovementsCreate";
+    }
+
     @PostMapping("/movements")
-    public Transaction postTransaction(@RequestBody Transaction transaction){
-        return this.service.postTransaction(transaction);    }
+    public String saveMovement(@ModelAttribute("transaction") Transaction transaction){
+        this.service.postTransaction(transaction);
+        return "redirect:/movements";
+    }
+
+    @GetMapping("/movements/edit/{id}")
+    public String FormEditUser(@PathVariable Long id, Model model){
+        model.addAttribute("transaction", this.service.getTransactionById(id));
+        return "MovementsEdit";
+    }
+
+    @PostMapping("/movements/{id}")
+    public String patchMovement(@PathVariable Long id, @ModelAttribute("transaction")Transaction transaction){
+        this.service.patchTransaction(id, transaction);
+        return "redirect:/movements";
+    }
+
     @GetMapping("/movements/{id}")
-    public Transaction getTransactionById(@PathVariable Long id){
-        return this.service.getTransactionById(id);
-    }
-    @PatchMapping("/movements/{id}")
-    public Transaction patchTransaction(@PathVariable Long id, @RequestBody Transaction transaction){
-        return this.service.patchTransaction(id,transaction);    }
-    @DeleteMapping("/movements/{id}")
-    public void deleteTransaction(@PathVariable Long id){
+    public String deleteMovement(@PathVariable Long id) {
         this.service.deleteTransaction(id);
+        return "redirect:/movements";
     }
-    @GetMapping("/enterprises/{id}/movements")
-    public List<Transaction> getTransactionByEnterprise(@PathVariable Long id){
-        return this.service.getTransactionByEnterprise(id);
-    }
+
 }
